937d32a5ac35036893322b562690c60b7184837b
JianZhirui <3264722570@qq.com>
2018-08-19 22:22:08 +0800

    update

--- chapter1/sprider.py
+++ chapter1/sprider.py
@@ -1,17 +1,25 @@
-from urllib import request, parse
+# from urllib import request, parse
 
-url = 'http://httpbin.org/post'
-headers = {
-    'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' ,
-    'Host':'httpbin.org'
-}
+# url = 'http://httpbin.org/post'
+# headers = {
+#     'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' ,
+#     'Host':'httpbin.org'
+# }
 
-dic = {
-    'name':'Ryan'
-}
+# dic = {
+#     'name':'Ryan'
+# }
 
-data = bytes(parse.urlencode(dic), encoding='utf-8')
-req = request.Request(url=url,data=data,headers=headers,method='POST')
-response = request.urlopen(req)
+# data = bytes(parse.urlencode(dic), encoding='utf-8')
+# req = request.Request(url=url,data=data,headers=headers,method='POST')
+# response = request.urlopen(req)
 
-print(response.read().decode('utf-8'))
+# print(response.read().decode('utf-8'))
+a = int(3.6)
+
+print(a)
+a = input('请输入：')
+
+b = int('3.14',base=10)
+
+print(b)
